9fae1081d6314daa61bec2a690181780fe8d95fb
Ebrahim Bonger <35973818+EbrahimBonger@users.noreply.github.com>
2020-07-24 13:14:15 -0400

    commit testing

--- Sort/src/AscendingBubbleSort.java
+++ Sort/src/AscendingBubbleSort.java
@@ -2,14 +2,14 @@ import java.lang.reflect.Array;
 import java.util.*;
 /**
  * Space: O(1)
- * Time: O(n^2) worst case scenario / O(n) if the array already sorted
+ * Time: O(n^2) worst case scenario / O(n) if the array is already sorted
  * **/
 public class AscendingBubbleSort {
     public static void main(String[] args) {
         int[] arr = {8, 5, 2, 9, 5, 6, 3};
         bubbleSort(arr);
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for(int a: arr){
+            System.out.print(a + " ");
         }
     }
 
