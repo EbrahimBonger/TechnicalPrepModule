d436b94d527e6f23dadcf792f70017ab05a757d0
Ebrahim Bonger <35973818+EbrahimBonger@users.noreply.github.com>
2020-07-25 16:23:04 -0400

    MinHeap completed

--- DataStructure/src/MinHeap.java
+++ DataStructure/src/MinHeap.java
@@ -1,4 +1,10 @@
-public class MinHeap {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class MinHeap {
     /**
      * Reference: https://www.youtube.com/watch?v=t0Cq6tVNRBA&list=PL2veGun5Zfn3WRwsmc7RD1DpCI-ip7jRX
      *            https://www.youtube.com/watch?v=2fA1FdxNqiE
@@ -15,40 +21,62 @@ public class MinHeap {
      * Finding the non-leaf nodes interval allows to build a heap data structure from the scratch using un-heapify array
      * **/
     public static void main(String[] args){
-        //int[] arr = {1, 14, 10, 2, 4, 9, 3};
-        int[] arr = {9, 6, 5, 0, 8, 2, 1, 3};
+
+        int[] arr = {48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41};
+        /**
+         * Convert int[] array to ArrayList
+         * **/
+        List<Integer> minHeap = Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+
+
         System.out.print("Before:");
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for(Integer e: minHeap){
+            System.out.print(e + " ");
         }
-        MinHeapify(arr, 0);
-        BuildMinHeap(arr);
+
+        /**
+         * Test the methods here
+         * **/
+        BuildMinHeap(minHeap);
+        insert(minHeap, 76);
+        System.out.println("top: " + peak(minHeap));
+        remove(minHeap);
+        System.out.println("top: " + peak(minHeap));
+        remove(minHeap);
+        System.out.println("top: " + peak(minHeap));
+        insert(minHeap, 87);
+
         System.out.print("After:");
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+        for(Integer e: minHeap){
+            System.out.print(e + " ");
         }
+
     }
+
+
+
     /**
      * The parent node with left and/or right child pass as a parameter to
      * analyze weather the child node(s) is smaller than the parent, if so
      * it will re-arrange by swapping the values
      * **/
-    public static void MinHeapify(int[] array, int i){
-
-        int heapSize = array.length-1;
+    public static void siftDown(List<Integer> array, int i){
+        //System.out.println("lastIdx: " + i);
+        int heapSize = array.size()-1;
         int left = (2*i)+1;
         int right = (2*i) +2;
         int smallest;
         /**
          * check weather the left child is larger than the parent
          * **/
-        if(left <= heapSize && array[left] < array[i]){
+        if(left <= heapSize && array.get(left) < array.get(i)){
             smallest = left;
         } else {smallest = i;}
         /**
          * check weather the right child is larger than the current smallest(parent or left child) node
          * **/
-        if(right <= heapSize && array[right] < array[smallest]){
+        if(right <= heapSize && array.get(right) < array.get(smallest)){
             smallest = right;
         }
         /**
@@ -66,28 +94,75 @@ public class MinHeap {
              * instead, it indicates it is the index where the smallest value was stored before swapped,
              * now, it will recurse back determine weather it needs to be heapify or not
              * **/
-            MinHeapify(array, smallest);
+            siftDown(array, smallest);
         }
     }
 
-    public static int[] BuildMinHeap(int[] array){
-        int heapSize = array.length;
+    /**
+     * Step 1: swap the root node with the last node of the MinHeap
+     * Step 2: remove the last index of the that currently has the value subjected to remove
+     * Step 3: perform a sift-down operation until the current node satisfy the MinHeap property
+     * **/
+    public static void remove(List<Integer> array){
+        if(array.isEmpty() == false){
+            int lastIdx = array.size()-1;
+            //System.out.println("size: " + array.size());
+            swap(array, 0, lastIdx);
+            array.remove(lastIdx);
+        }
+        siftDown(array, 0);
+    }
+
+    public static List<Integer> BuildMinHeap(List<Integer> array){
+        int heapSize = array.size();
         int numOfNonLeafNode = (heapSize/2)-1;
+        //System.out.println("Size: " + numOfNonLeafNode);
         /**
-         * The for llop has to run until 0 index to reach the root of the tree
+         * The for loop has to run until 0 index to reach the root of the tree
          * **/
         for(int i=numOfNonLeafNode; i>=0; i--){
-            MinHeapify(array, i);
+            siftDown(array, i);
         }
         return array;
     }
 
-    public static void swap(int[] array, int a, int b){
+    /**
+     * Step 1: add the coming value at the end (from left to right) of the Heap tree
+     * Step 2: perform a sift-Up operation until the current node satisfy the MinHeap property
+     * **/
+    public static void insert(List<Integer> array, int value){
+        array.add(value);
+        siftUp(array, array.lastIndexOf(array));
+
+    }
+    public static void siftUp(List<Integer> array, int currentIdx){
+        int parent = (currentIdx -1)/2;
+        while(currentIdx > 0 && array.get(currentIdx) < array.get(parent)){
+            swap(array, currentIdx, parent);
+            /**
+             * Now, since the swapped node go up by one and placed to a position where it's parent was,
+             * the previous parent position will become a new current index and
+             * calculate the current index's parent from that position
+             * **/
+            currentIdx = parent;
+            parent = (currentIdx-1)/2;
+        }
+    }
+
+
+    public static int peak(List<Integer> array){
+        int top = array.size()>0? array.get(0) : 0;
+        return top;
+
+
+    }
+
+    public static void swap(List<Integer> array, int a, int b){
         /**
          * The arguments are passed by reference not by value
          **/
-        int temp = array[a];
-        array[a] = array[b];
-        array[b] = temp;
+        Integer temp = array.get(a);
+        array.set(a, array.get(b));
+        array.set(b, temp);
     }
 }
