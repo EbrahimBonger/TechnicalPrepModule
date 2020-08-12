4a898791eb59c04afb561da62e5a29328b77d913
Ebrahim Bonger <35973818+EbrahimBonger@users.noreply.github.com>
2020-08-12 00:54:46 -0400

    ShiftedBinarySearch implemented

---
+++ Searching/src/ShiftedBinarySearch.java
@@ -0,0 +1,57 @@
+
+
+import java.util.*;
+
+class ShiftedBinarySearch {
+    /**
+     * Reference: https://www.algoexpert.io/questions/Shifted%20Binary%20Search
+     * **/
+
+    public static void main(String[] args){
+        int[] array = {61, 71, 72, 73, 0, 1, 21, 33, 45, 45};
+        int target = 33;
+        int idx = shiftedBinarySearch(array, target);
+        System.out.println("Idx: " + idx);
+    }
+
+    public static int shiftedBinarySearch(int[] array, int target) {
+        // Write your code here.
+        int[] shiftedIdx = returnShiftedIdx(array, target);
+        int start = shiftedIdx[0];
+        int end = shiftedIdx[1];
+        int searchResult = binarySearch(array, start, end, target);
+        return searchResult;
+    }
+    public static int binarySearch(int[] array,int start, int end, int target) {
+        // Write your code here.
+        return returnTargetIdx(array, start, end, target);
+    }
+    public static int returnTargetIdx(int[] arr, int start, int end, int target){
+        if(start > end){return -1;}
+        int mid = (start+end)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(target > arr[mid]){
+            //explore the right side
+            return returnTargetIdx(arr, mid+1, end, target);
+        } else {
+            return returnTargetIdx(arr, start, mid-1, target);
+        }
+    }
+    public static int[] returnShiftedIdx(int[] array, int target){
+        for(int i=1; i<array.length; i++){
+            if(array[i-1] > array[i]){
+                if(array[0] <= target && array[i-1] >= target){
+                    return new int[] {0, i-1};
+                }else if(array[i] <= target && array[array.length-1] >= target){
+                    return new int[] {i, array.length-1};
+
+                }
+
+            }
+        }
+        return new int[] {0, array.length-1};
+    }
+
+}
